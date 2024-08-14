726c6bddbbe7905ca687cb87046074698a29f415
André <andre.alves.silvazesez@gmail.com>
2024-08-14 20:14:38 -0300

    feat: add constructors that call super for exceptions

--- A-12-08-2024/src/evento/exception/ParticipantAlreadyExistsException.java
+++ A-12-08-2024/src/evento/exception/ParticipantAlreadyExistsException.java
@@ -1,6 +1,19 @@
 package evento.exception;
 
 public class ParticipantAlreadyExistsException extends Exception {
-    public ParticipantAlreadyExistsException(String s) {
+
+    public ParticipantAlreadyExistsException(String message, Throwable cause) {
+        super(message, cause);
+    }
+
+    public ParticipantAlreadyExistsException(Throwable cause) {
+        super(cause);
+    }
+
+    public ParticipantAlreadyExistsException(String message, Throwable cause, boolean enableSuppression, boolean writableStackTrace) {
+        super(message, cause, enableSuppression, writableStackTrace);
+    }
+
+    public ParticipantAlreadyExistsException() {
     }
 }
