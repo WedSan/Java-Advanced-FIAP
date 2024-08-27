4be964f569b980b8331aa1ac759c7eae542aa7c0
labsfiap <labsfiap@L2902MICRO16.fiap.com.br>
2024-08-26 22:11:59 -0300

    feat: start new spring boot project for new java class

---
+++ A-26-08-2024/src/main/java/net/andrelson/JavaWithSpringApplication.java
@@ -0,0 +1,13 @@
+package net.andrelson;
+
+import org.springframework.boot.CommandLineRunner;
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class JavaWithSpringApplication{
+
+	public static void main(String[] args) {
+		SpringApplication.run(JavaWithSpringApplication.class, args);
+	}
+}
