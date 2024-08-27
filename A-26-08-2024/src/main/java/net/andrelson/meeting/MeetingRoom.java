ce1770cae7b1f32c89b19d8cb89dfc7fcb60ad86
WedSan <andre.alves.silvazesez@gmail.com>
2024-08-27 19:12:39 -0300

    feat: add new attribute 'attributeName' and override equals

--- A-26-08-2024/src/main/java/net/andrelson/meeting/MeetingRoom.java
+++ A-26-08-2024/src/main/java/net/andrelson/meeting/MeetingRoom.java
@@ -2,9 +2,13 @@ package net.andrelson.meeting;
 
 import java.time.LocalDateTime;
 import java.util.Map;
+import java.util.Objects;
 import java.util.Set;
 
 public class MeetingRoom {
+
+	private int meetingRoomNumber;
+
 	private Map<LocalDateTime, Set<String>> meetings;
 	
 	private MeetingType meetingType;
@@ -12,9 +16,26 @@ public class MeetingRoom {
 	public MeetingRoom() {
 	}
 
-	public MeetingRoom(Map<LocalDateTime, Set<String>> meetings, MeetingType meetingType) {
-		this.meetings = meetings;
+	public MeetingRoom(int meetingRoomNumber, Map<LocalDateTime, Set<String>> meetings, MeetingType meetingType) {
+        this.meetingRoomNumber = meetingRoomNumber;
+        this.meetings = meetings;
 		this.meetingType = meetingType;
+	}
+
+	@Override
+	public boolean equals(Object o) {
+		if (this == o) return true;
+		if (o == null || getClass() != o.getClass()) return false;
+		MeetingRoom that = (MeetingRoom) o;
+		return meetingRoomNumber == that.meetingRoomNumber;
+	}
+
+	public int getMeetingRoomNumber() {
+		return meetingRoomNumber;
+	}
+
+	public void setMeetingRoomNumber(int meetingRoomNumber) {
+		this.meetingRoomNumber = meetingRoomNumber;
 	}
 
 	public Map<LocalDateTime, Set<String>> getMeetings() {
@@ -25,6 +46,12 @@ public class MeetingRoom {
 		this.meetings = meetings;
 	}
 
+	public void addMeeting(LocalDateTime start, Set<String> participants) {
+		meetings.put(start, participants);
+	}
+
+	public void removeMeeting(LocalDateTime meetingStart) {
+	}
 	public MeetingType getMeetingType() {
 		return meetingType;
 	}
