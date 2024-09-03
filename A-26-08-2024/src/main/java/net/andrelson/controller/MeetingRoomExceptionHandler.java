c8d175e363cab0bd5bb216ea670b89d70b1e5bfa
WedSan <andre.alves.silvazesez@gmail.com>
2024-09-03 01:41:29 -0300

    feat: add a class to handle meeting room exceptions

---
+++ A-26-08-2024/src/main/java/net/andrelson/controller/MeetingRoomExceptionHandler.java
@@ -0,0 +1,28 @@
+package net.andrelson.controller;
+
+import net.andrelson.dto.response.error.ErrorResponse;
+import net.andrelson.meeting.exception.MeetingRoomAlreadyExistsException;
+import org.springframework.http.HttpStatus;
+import org.springframework.http.ResponseEntity;
+import org.springframework.web.bind.annotation.ControllerAdvice;
+import org.springframework.web.bind.annotation.ExceptionHandler;
+import org.springframework.web.context.request.WebRequest;
+
+import java.time.LocalDateTime;
+
+@ControllerAdvice
+public class MeetingRoomExceptionHandler {
+
+    @ExceptionHandler(MeetingRoomAlreadyExistsException.class)
+    public ResponseEntity<ErrorResponse> handleMeetingRoomAlreadyExistsException(MeetingRoomAlreadyExistsException ex,
+                                                                                 WebRequest request) {
+        ErrorResponse errorResponse = new ErrorResponse(LocalDateTime.now(),
+                HttpStatus.CONFLICT.value(),
+                "Conflict",
+                "The meeting room already exists",
+                request.getDescription(false)
+                );
+
+        return ResponseEntity.status(HttpStatus.CONFLICT).body(errorResponse);
+    }
+}
