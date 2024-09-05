484ae960c3f22adf6920c697204077b7cfa626c4
WedSan <andre.alves.silvazesez@gmail.com>
2024-09-05 13:25:50 -0300

    feat: handle exceptions when meeting room is not found and when reservation is not found too

--- A-26-08-2024/src/main/java/net/andrelson/controller/exceptionHandler/MeetingRoomExceptionHandler.java
+++ A-26-08-2024/src/main/java/net/andrelson/controller/exceptionHandler/MeetingRoomExceptionHandler.java
@@ -1,7 +1,10 @@
 package net.andrelson.controller.exceptionHandler;
 
 import net.andrelson.dto.response.error.ErrorResponse;
+import net.andrelson.factory.ErrorResponseFactory;
 import net.andrelson.meeting.exception.MeetingRoomAlreadyExistsException;
+import net.andrelson.meeting.exception.MeetingRoomNotFoundException;
+import net.andrelson.meeting.exception.MeetingRoomReservationNotFoundException;
 import org.springframework.http.HttpStatus;
 import org.springframework.http.ResponseEntity;
 import org.springframework.web.bind.annotation.ControllerAdvice;
@@ -25,4 +28,17 @@ public class MeetingRoomExceptionHandler {
 
         return ResponseEntity.status(HttpStatus.CONFLICT).body(errorResponse);
     }
+
+    @ExceptionHandler({MeetingRoomNotFoundException.class, MeetingRoomReservationNotFoundException.class})
+    public ResponseEntity<ErrorResponse> handleMeetingRoomNotFoundException(Exception ex,
+                                                                            WebRequest request){
+        ErrorResponse errorResponse = ErrorResponseFactory.createErrorResponse(
+                HttpStatus.NOT_FOUND,
+                "Not Found",
+                ex.getMessage(),
+                request
+        );
+        return ResponseEntity.status(HttpStatus.NOT_FOUND).body(errorResponse);
+    }
+
 }
