864a7c80939bb1853fce04d99fdc7d6a0b73b377
WedSan <andre.alves.silvazesez@gmail.com>
2024-09-06 11:37:49 -0300

    refactor: return the appropriate status and body to client requests

--- A-26-08-2024/src/main/java/net/andrelson/controller/BookMeetingRoomController.java
+++ A-26-08-2024/src/main/java/net/andrelson/controller/BookMeetingRoomController.java
@@ -3,15 +3,13 @@ package net.andrelson.controller;
 import net.andrelson.dto.request.CancelReservationRequest;
 import net.andrelson.dto.request.EditReservationDateRequest;
 import net.andrelson.dto.request.MeetingRoomBookingRequest;
-import net.andrelson.dto.response.EditReservationDateResponse;
 import net.andrelson.dto.response.MeetingRoomResponse;
+import net.andrelson.mapper.MeetingRoomMapper;
 import net.andrelson.meeting.service.BookingManager;
 import net.andrelson.meeting.model.MeetingRoom;
 import org.springframework.http.HttpStatus;
 import org.springframework.http.ResponseEntity;
 import org.springframework.web.bind.annotation.*;
-
-import java.util.Map;
 
 @RestController
 @RequestMapping("meetingRoom/book")
@@ -30,24 +28,22 @@ public class BookMeetingRoomController {
                 meetingRoomBookingRequest.participantsName(),
                 meetingRoomBookingRequest.meetingType());
 
-        MeetingRoomResponse response = new MeetingRoomResponse(bookedMeetingRoom.getMeetingRoomNumber(),
-                Map.of(meetingRoomBookingRequest.meetingDate(), meetingRoomBookingRequest.participantsName()),
-                meetingRoomBookingRequest.meetingType());
+        MeetingRoomResponse response = MeetingRoomMapper.toResponse(bookedMeetingRoom);
 
         return ResponseEntity.status(HttpStatus.CREATED).body(response);
     }
 
     @PatchMapping("/{meetingRoomNumber}")
-    public ResponseEntity<EditReservationDateResponse> editReservationDate(@PathVariable int meetingRoomNumber,
+    public ResponseEntity<MeetingRoomResponse> editReservationDate(@PathVariable int meetingRoomNumber,
                                                                            @RequestBody EditReservationDateRequest editReservationDateRequest){
 
         MeetingRoom meetingRoom = bookingManager.editReservation(meetingRoomNumber,
                 editReservationDateRequest.oldReservationDate(),
                 editReservationDateRequest.newReservationDate());
 
-        EditReservationDateResponse response = new EditReservationDateResponse();
+        MeetingRoomResponse response = MeetingRoomMapper.toResponse(meetingRoom);
 
-        return ResponseEntity.status(HttpStatus.NO_CONTENT).build();
+        return ResponseEntity.status(HttpStatus.OK).body(response);
     }
 
     @DeleteMapping("/{meetingRoomNumber}")
