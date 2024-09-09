06a959402285958a5aa215587e64548b2795c002
WedSan <andre.alves.silvazesez@gmail.com>
2024-09-09 09:34:32 -0300

    feat: start task manager spring project

---
+++ Class-02-09-2024/src/main/java/fiap/class02092024/Class02092024Application.java
@@ -0,0 +1,13 @@
+package fiap.class02092024;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class Class02092024Application {
+
+    public static void main(String[] args) {
+        SpringApplication.run(Class02092024Application.class, args);
+    }
+
+}
---
+++ Class-02-09-2024/src/test/java/fiap/class02092024/Class02092024ApplicationTests.java
@@ -0,0 +1,13 @@
+package fiap.class02092024;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class Class02092024ApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
