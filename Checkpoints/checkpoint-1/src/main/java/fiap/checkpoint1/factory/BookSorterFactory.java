124f37d503cf725530a6153b901a775efaba76d3
WedSan <andre.alves.silvazesez@gmail.com>
2024-09-13 13:08:28 -0300

    fix: ensures that the specified instance is returned as expected

--- Checkpoints/checkpoint-1/src/main/java/fiap/checkpoint1/factory/BookSorterFactory.java
+++ Checkpoints/checkpoint-1/src/main/java/fiap/checkpoint1/factory/BookSorterFactory.java
@@ -2,13 +2,21 @@ package fiap.checkpoint1.factory;
 
 import fiap.checkpoint1.service.BookSortOption;
 import fiap.checkpoint1.service.BookSorter;
+import fiap.checkpoint1.service.bookSorter.BookAuthorSorter;
+import fiap.checkpoint1.service.bookSorter.BookCategorySorter;
 import fiap.checkpoint1.service.bookSorter.BookTitleSorter;
+import org.springframework.stereotype.Component;
 
+@Component
 public class BookSorterFactory {
 
     public BookSorter createBookSorter(BookSortOption option){
         switch (option){
-            case TITLE:
+            case CATEGORY:
+                return new BookCategorySorter();
+            case AUTHOR:
+                return new BookAuthorSorter();
+            default:
                 return new BookTitleSorter();
         }
     }
