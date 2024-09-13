36ed511625edd44c72b87379b87b8ac2dc2b5e1b
WedSan <andre.alves.silvazesez@gmail.com>
2024-09-13 12:56:12 -0300

    feat: add class to sort book by author

---
+++ Checkpoints/checkpoint-1/src/main/java/fiap/checkpoint1/service/bookSorter/BookAuthorSorter.java
@@ -0,0 +1,24 @@
+package fiap.checkpoint1.service.bookSorter;
+
+import fiap.checkpoint1.model.Book;
+import org.springframework.expression.spel.CompilablePropertyAccessor;
+
+import java.util.ArrayList;
+import java.util.Comparator;
+import java.util.List;
+
+public class BookAuthorSorter extends AbstractBookSorter {
+
+
+    @Override
+    public List<Book> sortBooks(List<Book> books) {
+        List<Book> booksOrdered = new ArrayList<>(books);
+        booksOrdered.sort(Comparator.comparing(book -> book.getName()));
+        return booksOrdered;
+    }
+
+    @Override
+    protected Comparator<Book> getComparator() {
+        return Comparator.comparing(book -> book.getAuthor());
+    }
+}
