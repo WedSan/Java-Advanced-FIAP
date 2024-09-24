288343079885be8f4913c8555b4a88b66c7f6f5c
WedSan <andre.alves.silvazesez@gmail.com>
2024-09-23 22:26:12 -0300

    feat: init new spring framework class project 23/09/2024

---
+++ class-23-09-2024/src/main/java/com/fiap/class_23_09_2024/Class23092024Application.java
@@ -0,0 +1,13 @@
+package com.fiap.class_23_09_2024;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class Class23092024Application {
+
+	public static void main(String[] args) {
+		SpringApplication.run(Class23092024Application.class, args);
+	}
+
+}
---
+++ class-23-09-2024/src/test/java/com/fiap/class_23_09_2024/Class23092024ApplicationTests.java
@@ -0,0 +1,13 @@
+package com.fiap.class_23_09_2024;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class Class23092024ApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
