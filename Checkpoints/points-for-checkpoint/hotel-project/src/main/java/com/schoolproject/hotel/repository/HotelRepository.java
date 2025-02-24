e628d48d5a83e46a139b55ef246b96af1286d109
WedSan <andre.alves.silvazesez@gmail.com>
2025-02-24 20:33:54 -0300

    feat: Add repository to interact with DB for Hotel Entity

---
+++ Checkpoints/points-for-checkpoint/hotel-project/src/main/java/com/schoolproject/hotel/repository/HotelRepository.java
@@ -0,0 +1,9 @@
+package com.schoolproject.hotel.repository;
+
+import com.schoolproject.hotel.model.HotelEntity;
+import org.springframework.data.jpa.repository.JpaRepository;
+import org.springframework.stereotype.Repository;
+
+@Repository
+public interface HotelRepository extends JpaRepository<HotelEntity, Long> {
+}
