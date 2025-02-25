7fc7b82380c83e6a2d3bb83ef23be842afaf04fb
WedSan <andre.alves.silvazesez@gmail.com>
2025-02-25 20:42:09 -0300

    feat: init hotel project spring security

---
+++ Checkpoints/points-for-checkpoint/hotel-project-spring-security/src/main/java/com/schoolproject/hotel_project_spring_security/HotelProjectSpringSecurityApplication.java
@@ -0,0 +1,13 @@
+package com.schoolproject.hotel_project_spring_security;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class HotelProjectSpringSecurityApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(HotelProjectSpringSecurityApplication.class, args);
+	}
+
+}
